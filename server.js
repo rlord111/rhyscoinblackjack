e4de232943e228165e70f727b0251b255056a7ec
rlord111 <rustyrhys1@gmail.com>
2025-07-26 06:45:21 +1000

    Add files via upload

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-// server.js
-
 const express = require('express');
-const path = require('path');
 const app = express();
+const path = require('path');
+require('dotenv').config();
 
-// Serve static files from /public
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Fallback to index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
